c4c5e19b4203ff05a179842f449139018caebbe0
podipod <35362368+podipod@users.noreply.github.com>
2021-07-11 18:47:17 +0800

    Update wireguard_install.sh

--- wireguard_install.sh
+++ wireguard_install.sh
@@ -135,14 +135,14 @@ add_user(){
     ipnum=$(grep Allowed /etc/wireguard/wg0.conf | tail -1 | awk -F '[ ./]' '{print $6}')
     newnum=$((10#${ipnum}+1))
     sed -i 's%^PrivateKey.*$%'"PrivateKey = $(cat temprikey)"'%' $newname.conf
-    sed -i 's%^Address.*$%'"Address = 10.10.0.$newnum\/32"'%' $newname.conf
+    sed -i 's%^Address.*$%'"Address = 10.10.0.$newnum\/24"'%' $newname.conf
 
 cat >> /etc/wireguard/wg0.conf <<-EOF
 [Peer]
 PublicKey = $(cat tempubkey)
-AllowedIPs = 10.77.77.$newnum/32
+AllowedIPs = 10.10.0.$newnum/24
 EOF
-    wg set wg0 peer $(cat tempubkey) allowed-ips 10.77.77.$newnum/32
+    wg set wg0 peer $(cat tempubkey) allowed-ips 10.10.0.$newnum/24
     echo -e "\033[37;41m添加完成，文件：/etc/wireguard/$newname.conf\033[0m"
     rm -f temprikey tempubkey
 }
