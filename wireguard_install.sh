9e0b6d7f11195a3e598e74e569a61e5d93363cd5
podipod <35362368+podipod@users.noreply.github.com>
2021-07-11 18:55:28 +0800

    Update wireguard_install.sh

--- wireguard_install.sh
+++ wireguard_install.sh
@@ -107,9 +107,11 @@ wireguard_install(){
 cat > /etc/wireguard/wg0.conf <<-EOF
 [Interface]
 PrivateKey = $s1
-Address = 10.77.0.1/16 
-PostUp   = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -A FORWARD -o wg0 -j ACCEPT; iptables -I FORWARD -s 10.10.0.1/24 -d 10.10.0.1/24 -j DROP; iptables -t nat -A POSTROUTING -o $eth -j MASQUERADE
-PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -D FORWARD -o wg0 -j ACCEPT; iptables -D FORWARD -s 10.10.0.1/24 -d 10.10.0.1/24 -j DROP; iptables -t nat -D POSTROUTING -o $eth -j MASQUERADE
+Address = 10.10.0.1/24 
+#PostUp   = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -A FORWARD -o wg0 -j ACCEPT; iptables -I FORWARD -s 10.10.0.1/24 -d 10.10.0.1/24 -j DROP; iptables -t nat -A POSTROUTING -o $eth -j MASQUERADE
+#PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -D FORWARD -o wg0 -j ACCEPT; iptables -D FORWARD -s 10.10.0.1/24 -d 10.10.0.1/24 -j DROP; iptables -t nat -D POSTROUTING -o $eth -j MASQUERADE
+PostUp   = iptables -A FORWARD -i %i -j ACCEPT; iptables -A FORWARD -o %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -D FORWARD -o %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
 ListenPort = $port
 DNS = 8.8.8.8
 MTU = 1420
