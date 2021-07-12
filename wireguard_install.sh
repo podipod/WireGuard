705bd6bcb53a0c949ece6e27ef1dc4e3b44f69e1
podipod <35362368+podipod@users.noreply.github.com>
2021-07-12 17:30:27 +0800

    Update wireguard_install.sh

--- wireguard_install.sh
+++ wireguard_install.sh
@@ -56,34 +56,6 @@ set_firewall() {
              "firewall-cmd --permanent --zone=public --add-port=$port/udp"
             "firewall-cmd --reload"
         fi
-    else
-        if  "iptables"; then
-            iptables -A INPUT -p udp --dport $port -j ACCEPT
-            iptables -A FORWARD -i wg0 -j ACCEPT
-            iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-            iptables-save > /etc/iptables.rules
-            if [ -d "/etc/network/if-up.d" ]; then
-                cat > /etc/network/if-up.d/iptables <<EOF
-#!/bin/sh
-/sbin/iptables-restore < /etc/iptables.rules
-EOF
-                chmod +x /etc/network/if-up.d/iptables
-            fi
-        fi
-        if  "ip6tables"; then
-            ip6tables -A INPUT -p udp --dport $port -j ACCEPT
-            ip6tables -A FORWARD -i wg0 -j ACCEPT
-            ip6tables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-            ip6tables-save > /etc/ip6tables.rules
-            if [ -d "/etc/network/if-up.d" ]; then
-                cat > /etc/network/if-up.d/ip6tables <<EOF
-#!/bin/sh
-/sbin/ip6tables-restore < /etc/ip6tables.rules
-EOF
-                chmod +x /etc/network/if-up.d/ip6tables
-            fi
-        fi
-    fi
 }
 #生成随机端口
 rand(){
